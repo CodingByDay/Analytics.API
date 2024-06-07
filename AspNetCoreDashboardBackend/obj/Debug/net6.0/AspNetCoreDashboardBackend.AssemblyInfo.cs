96910aee543c0427ec2ec0760b7a3baa24fe89b2
Janko Jovičić <jankojovicic351@gmail.com>
2024-06-07 14:20:03 +0200

    Gitignore

--- AspNetCoreDashboardBackend/obj/Debug/net6.0/AspNetCoreDashboardBackend.AssemblyInfo.cs
+++ AspNetCoreDashboardBackend/obj/Debug/net6.0/AspNetCoreDashboardBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreDashboardBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc042652ff1fc1a4bf8c11be612f0a1bb9f65af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4c2ac0589881f7cc6261a297a2a32d4a1fccfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreDashboardBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreDashboardBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
